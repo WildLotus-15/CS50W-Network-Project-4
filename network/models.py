c0cbb9f816c25570d7e2c07e621cb6ec8e58babe
WildLotus-15 <you@example.com>
2022-01-11 02:43:04 -0800

    Created Post model, user is able to create new Post, trying to move form on index page

---
+++ network/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Post(models.Model):
+    post = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author}: {self.post} on {self.date}"
